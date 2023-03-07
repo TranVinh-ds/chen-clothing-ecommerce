ac382619d30fcc83202118d6ab0ef9c18d7e1f74
TranVinh-ds <chenvinh2003@gmail.com>
2023-03-08 00:15:06 +0700

    create authentication page alongside sign in form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Home from './routes/home/Home';
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/Navigation';
-import SignIn from './routes/sign-in/SignIn';
+import Authentication from './routes/authentication/Authentication';
 
 function App() {
   const Shop = () => {
@@ -12,7 +12,7 @@ function App() {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
